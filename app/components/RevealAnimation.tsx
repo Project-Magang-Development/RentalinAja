4aac4ff030d103e2083b6012d803b9a894f54d3a
Kusuma <kusumadewantara3@gmail.com>
2024-06-18 12:15:32 +0800

    reveal animation

---
+++ app/components/RevealAnimation.tsx
@@ -0,0 +1,28 @@
+import React, { useRef, ReactNode } from "react";
+import { useInView } from "framer-motion";
+
+interface SectionProps {
+  children: ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section ref={ref}>
+      <div
+        ref={ref}
+        style={{
+          transform: isInView ? "none" : "translateY(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default Section;
